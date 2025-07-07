58a2efe42115d756121908b5b06028dff12138e5
ksundev <kshk08@hanyang.ac.kr>
2025-07-07 15:21:48 +0900

    refactor: todo-list 컴포넌트 비즈니스 로직을 app 컴포넌트로 이동

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <TodoHeader />
     <TodoInput />
-    <TodoList />
+    <TodoList :todoItems="todoItems" />
     <TodoFooter />
   </div>
 </template>
@@ -14,6 +14,22 @@ import TodoList from "./components/TodoList.vue";
 import TodoFooter from "./components/TodoFooter.vue";
 
 export default {
+  data() {
+    return {
+      todoItems: [],
+    };
+  },
+  created() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+          this.todoItems.push(
+            JSON.parse(localStorage.getItem(localStorage.key(i)))
+          );
+        }
+      }
+    }
+  },
   components: {
     TodoHeader,
     TodoInput,
